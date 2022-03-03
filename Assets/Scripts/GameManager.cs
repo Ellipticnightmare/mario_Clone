e4fd97c99f5e573923dc5979d96e1f8e50b2f3b0
Ellipticnightmare <31974144+Ellipticnightmare@users.noreply.github.com>
2022-03-03 14:57:06 +1100

    Increased readibility
    Added comments and regions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,3 +16,14 @@ public class GameManager : MonoBehaviour
         
     }
 }
+#region Externals
+#region Structs
+[System.Serializable]
+public struct keyBindingData //basic struct just to make things easier in the inspector
+{
+    public string commandName, keyName;
+}
+#endregion
+#region Classes
+#endregion
+#endregion
--- Assets/Scripts/marioController.cs
+++ Assets/Scripts/marioController.cs
@@ -3,29 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))] //Controls movement
+[RequireComponent(typeof(BoxCollider2D))] //detects Crouching collisions
+[RequireComponent(typeof(BoxCollider2D))] //detects Standing collisions
 public class marioController : MonoBehaviour
 {
-    public marioState MarioState = marioState.small;
-    public UnityEvent onDeath, coinPickup, lifeUp;
-    public keyBindingData[] keyBinds;
-    public int baseSpeed;
-    public GameObject visualHolder;
-    public BoxCollider2D standCol, crouchCol;
-    float hInput, vInput;
-    float movSpeed;
-    bool isGrounded;
-    Rigidbody2D rigid;
-    KeyCode right, left, up, down, jump, bButton;
+    #region Variables
+    #region Accessible
+    public marioState MarioState = marioState.small; //manages health
+    public UnityEvent onDeath, coinPickup, lifeUp; //runs events out to GameManager script, rather than needing references or Finds
+    public keyBindingData[] keyBinds; //lets us set custom keybindings, or even rebindable keys down the line.  Easier than changing script or Input Manager
+    public int baseSpeed; //Mario's base movement speed
+    public GameObject visualHolder; //Holds the sprites/animations, etc in a dedicated object for ease of manipulation
+    public BoxCollider2D standCol, crouchCol; //References the two colliders
+    #endregion
+    #region Protected
+    float hInput, vInput; //Creates axes based on vertical and horizontal movement
+    float movSpeed; //adjustable float based on basespeed and inputs
+    bool isGrounded; //Detects if we're standing on something, regardless of type, lets us jump off of enemies and such as well as ground
+    Rigidbody2D rigid; //Rigidbody reference
+    KeyCode right, left, up, down, jump, bButton; //Stores keycodes so they can be accessed by other areas of the script
+    marioState heldState = marioState.small; //holds the previous state for coming out of star
+    #endregion
+    #endregion
+
+    #region Functions
+    #region Base
     private void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>(); //Assign Rigidbody automatically
     }
     public void Update()
     {
-        #region KeyBinding
+        #region KeyBinding //Read keybinding data
         right = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyBinds[0].keyName);
         left = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyBinds[1].keyName);
         up = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyBinds[2].keyName);
@@ -34,39 +44,39 @@ public class marioController : MonoBehaviour
         bButton = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyBinds[5].keyName);
         #endregion
 
-        float lNorm = (Input.GetKey(left)) ? 1 : 0;
-        float rNorm = (Input.GetKey(right)) ? 1 : 0;
-        float uNorm = (Input.GetKey(up)) ? 1 : 0;
-        float dNorm = (Input.GetKey(down)) ? 1 : 0;
-        float bButtonF = (Input.GetKey(bButton)) ? 1 : 0;
+        float lNorm = (Input.GetKey(left)) ? 1 : 0; //Take in left input data
+        float rNorm = (Input.GetKey(right)) ? 1 : 0; //Take in right input data
+        float uNorm = (Input.GetKey(up)) ? 1 : 0; //Take in up input data
+        float dNorm = (Input.GetKey(down)) ? 1 : 0; //Take in down input data
+        float bButtonF = (Input.GetKey(bButton)) ? 1 : 0; //Take in Special input data
 
-        hInput = rNorm - lNorm;
-        vInput = uNorm - dNorm;
+        hInput = rNorm - lNorm; //Build horizontal axis
+        vInput = uNorm - dNorm; //Build vertical axis
 
-        if (Input.GetKey(down))
+        if (Input.GetKey(down)) //If crouching, prevent running, firing fireballs, etc
             bButtonF = 0;
-        movSpeed = (bButtonF > 0) ? baseSpeed * 1.5f : Input.GetKey(down) ? baseSpeed * .75f : baseSpeed;
+        movSpeed = (bButtonF > 0) ? baseSpeed * 1.5f : Input.GetKey(down) ? baseSpeed * .75f : baseSpeed; //Adjust speed based on running, crouching, etc
 
-        rigid.velocity = new Vector2(hInput * movSpeed, rigid.velocity.y);
+        rigid.velocity = new Vector2(hInput * movSpeed, rigid.velocity.y); //Set velocity for movement
 
         if (hInput != 0)
-            visualHolder.transform.localScale = new Vector3(hInput, 1, 1);
+            visualHolder.transform.localScale = new Vector3(hInput, 1, 1); //flip mario left or right depending on movement
 
-        standCol.enabled = (Input.GetKey(down)) ? false : true;
-        crouchCol.enabled = (Input.GetKey(down)) ? true : false;
+        standCol.enabled = (Input.GetKey(down)) ? false : true; //If crouching, turn off standing collider
+        crouchCol.enabled = (Input.GetKey(down)) ? true : false; //If standing, turn off crouching collider
     }
     public void FixedUpdate()
     {
-        int layerMask = 1 << 8;
-        layerMask = ~layerMask;
+        int layerMask = 1 << 8; //Read layers up to player layer
+        layerMask = ~layerMask; //Read all layers except for player layer
         RaycastHit upHit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out upHit, 1.1f, layerMask))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out upHit, 1.1f, layerMask)) //Detect what Mario bonks his head into
         {
             if (upHit.collider.tag == "block")
                 upHit.collider.gameObject.GetComponent<BlockController>().RunHit();
         }
         RaycastHit downHit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out downHit, 1.1f, layerMask))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out downHit, 1.1f, layerMask)) //Detect what Mario steps on, if stepping, make script know Mario is grounded
         {
             isGrounded = true;
             if (downHit.collider.tag == "enemy")
@@ -75,23 +85,20 @@ public class marioController : MonoBehaviour
         else
             isGrounded = false;
 
-        if (isGrounded && Input.GetKey(jump))
+        if (isGrounded && Input.GetKey(jump)) //Allow for jumping if Mario is grounded
         {
             rigid.AddForce(Vector2.up * movSpeed * 10);
         }
     }
-    public void UpdateMarioAppearance()
-    {
-
-    }
+    #endregion
     public void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag.ToString())
         {
-            case "killzone":
+            case "killzone": //Kill Mario upon falling through hole in ground
                 RunDeath();
                 break;
-            case "enemy":
+            case "enemy": //Update Mario's health based on size
                 switch (MarioState)
                 {
                     case marioState.small:
@@ -109,44 +116,50 @@ public class marioController : MonoBehaviour
                         break;
                 }
                 break;
-            case "mushroom":
-                MarioState = marioState.big;
+            case "mushroom": //Increase Mario's health when small
+                if (MarioState == marioState.small)
+                    MarioState = marioState.big;
                 UpdateMarioAppearance();
                 Destroy(collision.gameObject);
                 break;
-            case "flower":
+            case "flower": //Move to Flower form
                 MarioState = marioState.flower;
                 UpdateMarioAppearance();
                 Destroy(collision.gameObject);
                 break;
-            case "star":
+            case "star": //Move to Star form
+                heldState = MarioState;
                 MarioState = marioState.star;
                 UpdateMarioAppearance();
                 Destroy(collision.gameObject);
                 break;
-            case "coin":
+            case "coin": //Pick up coin
                 RunCoin();
                 Destroy(collision.gameObject);
                 break;
-            case "lifeUp":
+            case "lifeUp": //Gain extra life
                 RunLifeShroom();
                 Destroy(collision.gameObject);
                 break;
         }
     }
-    public void RunDeath() => onDeath.Invoke();
-    public void RunCoin() => coinPickup.Invoke();
-    public void RunLifeShroom() => lifeUp.Invoke();
-    public enum marioState
+    #region Custom
+    public void UpdateMarioAppearance()
+    {
+        //Logic to change Mario's appearance will go here
+    }
+    public void RunDeath() => onDeath.Invoke(); //Call to GameManager, tell them we died
+    public void RunCoin() => coinPickup.Invoke(); //Call to GameManager, tell them we picked up a coin
+    public void RunLifeShroom() => lifeUp.Invoke(); //Call to GameManager, tell them we gained a life
+    #endregion
+    #endregion
+    #region States
+    public enum marioState //health state
     {
         small,
         big,
         star,
         flower
     };
-}
-[System.Serializable]
-public struct keyBindingData
-{
-    public string commandName, keyName;
+    #endregion
 }
