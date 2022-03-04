3937697677c6158610ecd10501cad80859b6c635
hein <hein@grobler.dev>
2022-03-04 19:56:29 +1100

    Added Goombas + fixes
    goombas movement added
    removed a semicolon from marioController that was causing issues
    Created an enemy layer
    added stairblocks with collision for personal testing
    both prefabs are under 'Hein', happy for these to be moved to a prefabs folder

---
+++ Assets/Scripts/GoombaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaController : MonoBehaviour
+{
+    public float speed; //the goombas speed
+    public float flipDistance; //how far the raycast looking for walls goes, 0.32 should be fine
+    private int dir; // direction goomba is going in
+    private int layerMask; //bitmask to choose layer to ignore in raycasts
+
+    private void Start()
+    {
+        dir = -1;   //goes to the left initially
+        layerMask = 1 << 9; 
+        layerMask = ~layerMask; //bitmask that tells the raycast to ignore layer 9 (enemy layer)
+    }
+
+    public void Update()
+    {
+        //checking for walls ahead with a raycast, flips direction if found
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(dir,0.0f), flipDistance, layerMask);
+        if (hit.collider != null)
+        {
+            dir *= -1;
+        }
+        //movement!
+        transform.Translate(new Vector2(dir * speed * Time.deltaTime, 0.0f));
+    }
+
+}
+
