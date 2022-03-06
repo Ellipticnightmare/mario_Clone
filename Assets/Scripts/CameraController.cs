7763c411d6287b7aa49980a12cb66a90b51ea1c8
hein <hein@grobler.dev>
2022-03-06 16:35:51 +1100

    Camera implemented
    camera now follows mario properly and behaves like the original mario camera
    Walking backwards is properly blocked

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    private float furthestReached;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        furthestReached = target.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.transform.position.x > furthestReached)
+        {
+            furthestReached = target.transform.position.x;
+        }
+    }
+    
+    void LateUpdate()
+    {
+        transform.position = new Vector3(furthestReached + 1.0f, 0, transform.position.z);
+    }
+}
