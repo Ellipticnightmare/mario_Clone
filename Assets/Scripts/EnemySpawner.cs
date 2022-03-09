f19c1e88d3b1243df6bf2d18e432a882503da945
hein <hein@grobler.dev>
2022-03-09 16:16:12 +1100

    Enemy Spawner and Compatible Camera are implemented

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyToSpawn;
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("MainCamera"))
+        {
+            Instantiate(enemyToSpawn, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
